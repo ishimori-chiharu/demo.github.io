40c566511386e132f7f4195b12e0d945468f3ed6
ishimori-chiharu <118794920+ishimori-chiharu@users.noreply.github.com>
2023-01-24 08:49:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ let total = 0; //正答
 // [START]ボタンクリック
 function start(){
     let maxNo = 5; //足合せ数
-    console.log("call start");
+    //console.log("call start");
 
     total = makeQuestion(maxNo);
-    console.log("total at start func:", total);
+    //console.log("total at start func:", total);
 }
 
 // [Answer Check]ボタンクリック
 function check(){
     let ansVal = document.getElementById('answer');
-    console.log("answer:", ansVal);
+    //console.log("answer:", ansVal);
 
     const interval = document.getElementById('interval').value;
-    console.log("interval:", interval);
+    //console.log("interval:", interval);
 
     const digit = document.getElementById('digit').value;
-    console.log("digit:", digit);
+    //console.log("digit:", digit);
 
 
     //ページを変更
@@ -45,17 +45,17 @@ function makeQuestion(maxNo){
     //const waitMSec = 800;  //表示間隔
 
     const waitMSec = document.getElementById('interval').value * 1000;
-    console.log("interval:", waitMSec);
+    //console.log("interval:", waitMSec);
 
     const digit = document.getElementById('digit').value;
-    console.log("digit:", digit);
+    //console.log("digit:", digit);
 
-    console.log("call question");
+    //console.log("call question");
 
     let lastNum = 0;
     for(let i = 0; i < maxNo; i++){
         val = Math.floor(Math.random()*(10**digit));
-        while(val === lastNum && val > 0){
+        while(val === lastNum || val === 0){
             val = Math.floor(Math.random()*(10**digit));
         }
         lastNum = val;
@@ -63,8 +63,8 @@ function makeQuestion(maxNo){
         retNum += val;
         quesNums.push(val);
     }
-    console.log("retNum:", retNum);
-    console.log("quesNums:", quesNums);
+    console.log("問題:", quesNums);
+    console.log("答え:", retNum);
 
     //画面に表示
     for(let i = 0; i < quesNums.length; i++){
@@ -105,7 +105,7 @@ function syncDelay(msec){
 
 //数字の表示
 function dispNums(num, msec){
-    console.log("flash num:", num);
+    //console.log("flash num:", num);
     const mainView = document.getElementById('main');   //表示位置のドキュメント
     mainView.textContent = num;
     syncDelay(msec);
@@ -114,14 +114,14 @@ function dispNums(num, msec){
 
 function back(interval, digit){
     //window.location.href = 'index.html';
-    console.log("Go back to main page.")
-    console.log("interval =",interval);
-    console.log("digit =",digit);
+    //console.log("Go back to main page.")
+    //console.log("interval =",interval);
+    //console.log("digit =",digit);
     window.location.href = 'index.html?name=' + interval + '&name2=' +  digit;
 }
 
 function dispAnswer(collectNum){
-    console.log("call disp answer! total = ", collectNum);
+    //console.log("call disp answer! total = ", collectNum);
     let dispStr = `正しい答えは「${collectNum}」です`;
     //document.getElementById("collect_answer").innerHTML = dispStr;
     //document.getElementById("collect_answer").textContent = dispStr;
@@ -131,8 +131,9 @@ function dispAnswer(collectNum){
 
 
 function resetSetting(interval, digit){
-    console.log("call reset settings. interval = ", interval);
+    //console.log("call reset settings. interval = ", interval);
     document.getElementById('interval').value =  interval;
-    console.log("call reset settings. digit = ", digit);
+    //console.log("call reset settings. digit = ", digit);
     document.getElementById('digit').value =  digit;
 }
+
